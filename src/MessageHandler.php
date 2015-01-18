10b2ae61f8c6bfdfd963b47049d82a9873ed0066
Niklas Keller <me@kelunik.com>
2015-01-18 02:23:11 +0100

    Remove room_users from message join

--- src/MessageHandler.php
+++ src/MessageHandler.php
@@ -15,7 +15,6 @@ SELECT
 	u2.name AS replyUserName
 FROM (SELECT * FROM `messages` WHERE roomId = ? && {$where} ORDER BY {$order} LIMIT {$limit}) AS m
 	JOIN `users` AS u ON (m.userId = u.id)
-	JOIN `room_users` AS ru ON (ru.roomId = m.roomId && ru.userId = u.id)
 	LEFT JOIN (SELECT m.id AS messageId, u.id, u.name FROM messages AS m, users AS u WHERE m.userId = u.id) AS u2 ON (u2.messageId = m.replyTo)
 	LEFT JOIN (SELECT messageId, COUNT(*) AS `count` FROM `message_stars` GROUP BY messageId) AS stars ON (stars.messageId = m.id)
 	LEFT JOIN message_stars AS ms ON (ms.userId = ? && ms.messageId = m.id)
