6ef02a50ed96d87361ec79382d3eb220ce72278f
Niklas Keller <me@kelunik.com>
2015-01-25 22:11:44 +0100

    Right icon order, move to extra file

--- html/chat.php
+++ html/chat.php
@@ -8,15 +8,7 @@
 		console.time("pageload");
 	</script>
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-	<meta name="mobile-web-app-capable" content="yes">
-	<link rel="icon" sizes="192x192" href="/img/icon_192x192.png">
-	<link rel="icon" sizes="128x128" href="/img/icon_128x128.png">
-	<link rel="icon" sizes="40x40" href="/img/logo_40x40.png">
-	<link rel="icon" sizes="16x16" href="/img/icon.ico">
-	<link rel="apple-touch-icon" sizes="128x128" href="/img/icon_128x128.png">
-	<link rel="apple-touch-icon-precomposed" sizes="128x128" href="/img/icon_128x128.png">
-	<link rel="manifest" href="/manifest.json">
+	<?php require TEMPLATE_DIR . "head_meta.php"; ?>
 
 	<script>
 		var url = "<?=DEPLOY_HTTPS ? "wss" : "ws"?>://<?=DEPLOY_AUTHORITY?>/chat";
--- html/settings.php
+++ html/settings.php
@@ -4,15 +4,7 @@
 	<title>Settings – t@lk</title>
 	<meta charset="utf-8">
 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-	<meta name="mobile-web-app-capable" content="yes">
-	<link rel="icon" sizes="192x192" href="/img/icon_192x192.png">
-	<link rel="icon" sizes="128x128" href="/img/icon_128x128.png">
-	<link rel="icon" sizes="40x40" href="/img/logo_40x40.png">
-	<link rel="icon" sizes="16x16" href="/img/icon.ico">
-	<link rel="apple-touch-icon" sizes="128x128" href="/img/icon_128x128.png">
-	<link rel="apple-touch-icon-precomposed" sizes="128x128" href="/img/icon_128x128.png">
-	<link rel="manifest" href="/manifest.json">
+	<?php require TEMPLATE_DIR . "head_meta.php"; ?>
 
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:400,700|Source+Code+Pro">
 	<link rel="stylesheet" href="/css/all.min.css?v=<?= CSS_VERSION ?>">
