27a225643a42da1ec057c011193617d911daf930
Niklas Keller <me@kelunik.com>
2015-08-29 23:04:28 +0200

    Embed userdata to message history

--- src/Commands/Rooms/History.php
+++ src/Commands/Rooms/History.php
@@ -6,13 +6,16 @@ use Kelunik\Chat\Boundaries\Request;
 use Kelunik\Chat\Boundaries\User;
 use Kelunik\Chat\Command;
 use Kelunik\Chat\Storage\MessageStorage;
+use Kelunik\Chat\Storage\UserStorage;
 use function Kelunik\Chat\createPaginationResult;
 
 class History extends Command {
     private $messageStorage;
+    private $userStorage;
 
-    public function __construct(MessageStorage $messageStorage) {
+    public function __construct(MessageStorage $messageStorage, UserStorage $userStorage) {
         $this->messageStorage = $messageStorage;
+        $this->userStorage = $userStorage;
     }
 
     public function execute(Request $request, User $user) {
@@ -23,6 +26,18 @@ class History extends Command {
         $args->cursor = $args->cursor ?? -1;
 
         $messages = yield $this->messageStorage->getHistory($args->id, $args->cursor, $args->asc);
+
+        $userIds = array_column($messages, "user_id");
+        $users = [];
+
+        foreach (yield $this->userStorage->getByIds($userIds) as $user) {
+            $users[$user->id] = $user;
+        }
+
+        foreach ($messages as &$message) {
+            $message->user = $users[$message->user_id] ?? null;
+            unset($message->user_id);
+        }
 
         return createPaginationResult($messages);
     }
